845169050da6f9d68e5a6b641852bd1be9406445
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2023-09-08 11:40:39 +0530

    added footer and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import Background from './Background.jsx';
+import React from "react";
+import Background from "./Background.jsx";
+import NavBar from "./Components/NavBar.jsx";
+import Footer from "./Components/Footer.jsx";
 const App = () => {
-    return (
-        <>
-            <Background />
-        </>
-    )
-}
+  return (
+    <>
+      <Background />
+      <NavBar />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
